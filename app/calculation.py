ea07bc55733693f78273a7defc6a77c0079db432
avduryagin <avduryagin@mail.ru>
2022-07-18 11:52:36 +0300

    Изменен порядок парсинга json.

--- app/calculation.py
+++ app/calculation.py
@@ -12,10 +12,16 @@ class Calculation(Resource):
         self.models=pipeml
 
     def post(self,*args,**kwargs):
-        x = json.dumps(request.json)
-        data=x['data']
-        args=x['kwargs']
-        res=self.models.predict(data,drift=args['drift'])
+        #x = json.dumps(request.json)
+        x=json.loads(request.json)
+        try:
+            data=x['data']
+            args=x['kwargs']
+            res = self.models.predict(data, drift=args['drift'])
+        except(KeyError):
+            res = self.models.predict(x)
+
+
         return res
 
 api.add_resource(Calculation, "/calc/")
