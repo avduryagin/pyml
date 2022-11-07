124c5ad242208b75da3af9629360ce29cdf12096
avduryagin <avduryagin@mail.ru>
2022-11-07 17:51:07 +0300

    Доработаны классы и методы модуля remtime

--- app/calclib/remtime.py
+++ app/calclib/remtime.py
@@ -1,4 +1,19 @@
 import numpy as np
+from scipy.stats import norm
+import scipy.optimize as optim
+import json
+from functools import singledispatch
+
+@singledispatch
+def to_serializable(val):
+    """Used by default."""
+    return str(val)
+
+
+@to_serializable.register(np.float32)
+def ts_float32(val):
+    """Used if *val* is an instance of numpy.float32."""
+    return np.float64(val)
 
 class rtd:
     def __init__(self):
@@ -10,6 +25,7 @@ class rtd:
         self.smin=4.
         self.measurements=np.array([])
 
+
     def fit(self,measurements=np.array([]),t=1.,s=8.,s0=4.):
         if (measurements is not None) and type(measurements) is np.ndarray:
             nneg=np.where(measurements<0)[0]
@@ -19,13 +35,12 @@ class rtd:
             self.sigma=self.measurements.std()
             self.smin=self.measurements.min()
             self.mean = self.measurements.mean()
+
         assert t>=0,"Задан отрицательный возраст трубопровода"
         assert s > 0, "Задана нулевая или отрицательная номинальная толщина стенки "
         assert s0 > 0, "Задана нулевая или отрицательная отбраковочная толщина стенки "
         self.t=t
         self.s=s
-        self.s0=s0
-
 
 
     def value(self)->np.float32:
@@ -47,14 +62,274 @@ class rtd:
             tau=0
         return tau
 
+    def predict(self,s_=8.,ds=0.04,m=0.05,q=0.99,z=100):
+
+        assert (self.measurements.size >= 4), "Задано недотаточное для выполнения расчетов количество измерений"
+        assert s_>=0,"Задана нулевая или отрицательная расчетная толщина стенки"
+        assert ds > 0, "Задано отрицательое относительное начальное (технологическое) среднеквадратическое отклонение стенки"
+        assert m >= 0, "Задана отрицательное число допустимых отказавших элементов в год на 1 км длины "
+        assert z >= 0, "Задана отрицательное число элементов трубопровода на 1 км"
+        assert (q >= 0) and (q<1), "Задана некоректная доверительная вероятность. Значение должно быть положительным меньше 1. "
+
+
+        n=self.measurements.size
+        self.z=z
+        self.s_=s_
+        self.q=q
+        self.m=m
+        self.ds2=ds**2
+        self.dmean=1-self.mean/self.s
+        self.su=self.sigma/self.s
+        self.quantile=norm.ppf(q)
+        self.maxsq2=(self.su*(1+self.quantile/(np.power(2*n-8,0.5))))**2
+        self.delta=self.dmean+self.quantile*(self.su/np.power(n-2,0.5))
+        self.sk2=(self.maxsq2-self.ds2)/(self.t**2)
+        self.dr=1-self.s_/self.s
+        self.r=self.delta/self.t
+        self.dtau=self.dr-self.delta
+        self.epsilon=self.m/self.z
+        self.args=[]
+
+
+        def value(i=0)->np.float32:
+            x1=self.dtau-self.r*(i)
+            x2=x1-self.r
+            ksi=self.t+i
+            i2=(ksi)**2
+            i3=i2+2*ksi+1
+            y1=np.power(self.sk2*i2+self.ds2,0.5)
+            y2=np.power(self.sk2*i3+self.ds2,0.5)
+            w1=x1/y1
+            w2=x2/y2
+            self.args.append([w1,w2])
+            #print(w1)
+            #print(w2)
+            return np.abs(self.epsilon-(norm.cdf(w1)-norm.cdf(w2)))
+
+        return value
+
+    def get_time(self,s_=8.,ds=0.04,m=0.05,q=0.99,z=100,tol=1e-2):
+        fun=self.predict(s_=s_,ds=ds,m=m,q=q,z=z)
+        val=optim.minimize_scalar(fun,method='bounded',bounds=(0,30),tol=tol)
+        return val
+
+
+
+
+
+class techstate:
+    def __init__(self):
+        self.s=8.
+        self.s0=4.
+        self.tfirst=0.
+        self.time=np.array([],dtype=np.float32)
+        self.width=np.array([],dtype=np.float32)
+        self.values = np.array([], dtype=np.float32)
+        self.sa=np.array([],dtype=np.int16)
+        self.vcorr=0.
+        self.vcorr_mean=0.
+        self.vcorr_fact=0.
+        self.vcorr_max=0.
+
+    def concatinate(self,l):
+        def get_length(l)->np.int32:
+            if l is None:
+                return 0
+            if (type(l) == list) or (type(l) == tuple):
+                n = len(l)
+            elif type(l) == np.ndarray:
+                n = l.shape[0]
+            else: n=0
+            return n
+
+
+        n=get_length(l)
+        length=np.array([get_length(x) for x in l],dtype=np.int32)
+        m = np.max(length)
+        if m==0:
+            if type(l)==np.ndarray:
+                return l.reshape(-1,1)
+            else:
+                return np.array(l,dtype=np.float32).reshape(-1,1)
+
+
+
+        array=np.empty(shape=(n,m),dtype=np.float32)
+        array.fill(np.nan)
+        i=0
+        while i<n:
+            l_=l[i]
+            size=length[i]
+            if 0==size:
+                array[i,0]=l_
+                i+=1
+                continue
+            for j,v in enumerate(l_):
+                array[i,j]=v
+            i+=1
+        return array
+
+
+
+    def fit(self, s=np.array(8.), time=np.array([]), width=np.array([]), tfirst=np.array(0.), s0=np.array(4.)):
+
+        if (time is not None) and type(time) is np.ndarray and (width is not None) and type(width) is np.ndarray:
+            wneg=np.where(width<0)[0]
+            tneg = np.where(time < 0)[0]
+            assert wneg.shape[0]==0,'Некоторые значения измерений заданы отрицательными'
+            assert tneg.shape[0] == 0, 'Некоторые значения времени заданы отрицательными'
+        else:
+            assert False, "Задан некорректный тип данных. Ожидается numpy.ndarray"
+        assert tfirst>=0,"Задан отрицательный возраст первой аварии"
+        assert s > 0, "Задана нулевая или отрицательная номинальная толщина стенки "
+        assert s0 > 0, "Задана нулевая или отрицательная отбраковочная толщина стенки "
+
+
+
+        if (time.shape[0]==width.shape[0]) and time.shape[0]>0:
+            self.time=time
+            self.width=width
+        elif (time.shape[0]>0) and (width.shape[0]>0) and (time.shape[0]!=width.shape[0]):
+            n=min(time.shape[0],width.shape[0])
+            self.time=time[:n]
+            self.width=width[:n]
+        if self.time.shape[0]>0:
+            self.sa=np.argsort(self.time)
+            self.time=self.time[self.sa]
+            self.width=self.width[self.sa]
+        self.tfirst=tfirst
+        self.s=s
+        self.s0=s0
+
+    def get_samples(self,i: np.int16) -> np.ndarray:
+        array = np.zeros(4)
+        if i >= self.time.shape[0]:
+            return array
+        index = -(i + 1)
+        cwidth = np.nanmin(self.width[index])
+        ctime = self.time[index]
+        array[0] = ctime
+        array[1] = cwidth
+        try:
+            pwidth = np.nanmin(self.width[index - 1])
+            ptime = self.time[index - 1]
+            array[2] = ptime
+            array[3] = pwidth
+
+        except IndexError:
+            array[3]=self.s
+            return array
+        finally:
+            return array
+
+    def get_values(self):
+        def get_vcorr(cwidth=1,ctime=1,pwidth=0,ptime=0):
+            dt = ctime - ptime
+            if dt > 0:
+                vcorr = (pwidth - cwidth) / dt
+                if vcorr > 0:
+                    return vcorr
+            return 0
+
+        def get_vmax():
+            values=[]
+            i=0
+            maxval=0
+            index=0
+            while i<self.time.shape[0]:
+                smaple = self.get_samples(i)
+                ctime = smaple[0]
+                cwidth = smaple[1]
+                ptime = smaple[2]
+                pwidth = smaple[3]
+                val=get_vcorr(cwidth=cwidth, ctime=ctime, pwidth=pwidth, ptime=ptime)
+                values.append(val)
+                if maxval<val:
+                    maxval=val
+                    index=i
+                i+=1
+            return index,np.array(values,dtype=np.float32)
+
+
+        if self.time.shape[0]==0:
+            return self.vcorr,self.vcorr_mean,self.vcorr_fact,self.vcorr_max
+        latest=self.get_samples(0)
+        ctime = latest[0]
+        cwidth=latest[1]
+        #ptime = latest[2]
+        #pwidth=latest[3]
+
+        index,values=get_vmax()
+        self.values=values
+        self.vcorr=values[0]
+        self.vcorr_max=values[index]
+
+
+        if ctime>0:
+            vcorr_mean=(self.s-cwidth)/ctime
+            if vcorr_mean>0:
+                self.vcorr_mean=vcorr_mean
+
+        if self.tfirst>0:
+            vcorr_fact=(cwidth-self.s0)/self.tfirst
+            if vcorr_fact>0:
+                self.vcorr_fact=vcorr_fact
+
+        return self.vcorr,self.vcorr_mean,self.vcorr_fact,self.vcorr_max
+
+
+
+
 def predict(data,*args,**kwargs)->np.float32:
-    measurements = np.array(data['measurements'], dtype=np.float32)
-    s = np.array(data['s'], dtype=np.float32)
-    s0 = np.array(data['s0'], dtype=np.float32)
-    t = np.array(data['t'], dtype=np.float32)
+    measurements = np.array(data['measurements'], dtype=np.float32) #последние измерения дефектоскопии на трубопроводе.
+    s = np.array(data['s'], dtype=np.float32) #номинальная толщина.
+    sestimated = np.array(data['sestimated'], dtype=np.float32) #расчетная толщина.
+    tol = np.array(data['tolerance'], dtype=np.float32)#точность расчетов
+    m = np.array(data['acceptable'], dtype=np.float32)  #допустимое число отказов в год на 1 км длины
+    z = np.array(data['nelements'], dtype=np.float32)  # число элементов трубопровода на на 1 км длины (по умолчанию 100)
+    q = np.array(data['proba'], dtype=np.float32)  # доверительная вероятность
+    #- 0,99 – для напорных нефтепроводов, газопроводов и водоводов высокого давления,
+    #- 0,95 – для нефтесборных коллекторов, трубопроводов выкидных линий и водоводов низкого давления.
+
+    ds = np.array(data['sigma0'], dtype=np.float32)  # относительное среднее квадратичное начальное
+    # отклонение толщины стенки трубы в начальный момент,
+    # равное 0,04 для бесшовных труб и 0,03 – для сварных труб;
+    s0 = np.array(data['s0'], dtype=np.float32) #отбраковочная толщина.
+    t = np.array(data['t'], dtype=np.float32) #возраст трубопровода на момент последней диагностики.
+    tfirst=np.array(data['tfirst'], dtype=np.float32) #возраст перой аварии по коррозии.
+    time=np.array(data['samples']['time'], dtype=np.float32) # массив, содержащий возраст трубопровода на момент проведения диагностик.
+    width = np.array(data['samples']['width'], dtype=np.float32) #измерения дефектоскопии в контрольной точке,
+                                                                # соотнесенные с массивом time.
+
     #undersampling = np.array(kargs['undersampling'], dtype=bool)
 
-    model=rtd()
-    model.fit(measurements=measurements,t=t,s=s,s0=s0)
-    value=model.value()
-    return value
+    results=dict({"probab_rtime":0,"vcorr_by_meas":0,"vcorr_mean":0,"vcorr_mean_fact":0,"vcorr_max":0,"predicted_rtime":0,"log":""})
+    try:
+
+        model=rtd()
+        model.fit(measurements=measurements,t=t,s=s,s0=s0)
+        condtau=model.value()
+        results["probab_rtime"]=condtau
+        val = model.get_time(s_=sestimated, tol=tol,q=q,z=z,m=m,ds=ds)
+        tau=val['x']
+        results["predicted_rtime"]=tau
+        tech = techstate()
+        width = tech.concatinate(width)
+        tech.fit(width=width, time=time, s0=s0, s=s, tfirst=tfirst)
+        vcorr,vcorr_mean,vcorr_fact,vcorr_max=tech.get_values()
+        results["vcorr_by_meas"]=vcorr
+        results["vcorr_mean"] = vcorr_mean
+        results["vcorr_mean_fact"] = vcorr_fact
+        results["vcorr_max"] = vcorr_max
+
+    except AssertionError as error:
+        results['log']=error.args[0]
+
+
+    #tojs=results
+    tojs = json.dumps(results, default=to_serializable)
+
+
+    return tojs
+
+
